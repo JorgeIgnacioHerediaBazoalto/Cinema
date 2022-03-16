e580c9ce766087fa07c81f7be0dbce6ff19231bd
Elbeto379 <hidalgososajuancarlos@gmail.com>
2022-03-16 19:31:15 -0400

    Inicialización de Butaca, fila y sala

--- src/Butaca.java
+++ src/Butaca.java
@@ -1,2 +1,20 @@
 public class Butaca {
+    int numero;
+    boolean ocupado;
+    public Butaca(int numero,boolean ocupado){
+        this.numero=numero;
+        this.ocupado=ocupado;
+    }
+    public boolean isOcupado() {
+        return ocupado;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public String getInfoButaca() {
+        String info = "";
+        info += "Numero de butaca:\t\t" + getNumero() + "\n";
+        info += "Disponibilidad:\t" + isOcupado() + "\n";
+        return info;
+    }
 }
