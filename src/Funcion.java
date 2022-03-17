febb1ba364f7440093d710826e49d0e2aa1185d3
SebastianBarra <barra20sebastian02p19@gmail.com>
2022-03-16 20:23:46 -0400

    implementacion de pelicula, funcion y GeneroPelicula

--- src/Funcion.java
+++ src/Funcion.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+
 public class Funcion {
 
-    //jhsaonb fabhs
-    //
+    Pelicula peliculas;
+    String hora;
+    String dia;
+    Sala sala;
+    String tipoPelicula;
+    int precio2D;
+    int precio3D;
+
+    public Funcion(Pelicula peliculas, String hora, String dia, Sala sala, String tipoPelicula, int precio2D, int precio3D)
+    {
+        this.peliculas = peliculas;
+        this.hora = hora;
+        this.dia = dia;
+        this.sala = sala;
+        this.tipoPelicula = tipoPelicula;
+        this.precio2D = precio2D;
+        this.precio3D = precio3D;
+    }
+
+    public void mostrarFuncion()
+    {
+
+    }
 }
